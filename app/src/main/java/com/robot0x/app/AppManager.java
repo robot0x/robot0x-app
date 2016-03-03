cbda2aebe2fc1e77f7fb5fe6a02c9529fc812c7b
robot0x <mr.chenja@gmail.com>
2016-03-03 18:19:24 +0800

    add BaseApplication AppManager AppStart

---
+++ app/src/main/java/com/robot0x/app/AppManager.java
@@ -0,0 +1,115 @@
+package com.robot0x.app;
+
+import android.app.Activity;
+
+import java.util.Stack;
+
+/**
+ * activity堆栈式管理
+ * Created by Jackie on 2016/3/3.
+ */
+public class AppManager {
+    private static Stack<Activity> activityStack;
+    private static AppManager instance;
+
+    private AppManager() {}
+
+    /**
+     * 单一实例
+     * @return
+     */
+    public static AppManager getAppManager() {
+        if (instance == null) {
+            instance = new AppManager();
+        }
+        return instance;
+    }
+
+    /**
+     * 添加Activity到堆栈
+     * @param activity
+     */
+    public void addActivity(Activity activity) {
+        if (activityStack == null) {
+            activityStack = new Stack<Activity>();
+        }
+        activityStack.add(activity);
+    }
+
+    /**
+     * 获取当前Activity（堆栈中最后一个压入的）
+     * @return
+     */
+    public Activity currentActivity() {
+        Activity activity = activityStack.lastElement();
+        return activity;
+    }
+
+    /**
+     * 结束当前Activity（堆栈中最后一个压入的）
+     */
+    public void finishCurrentActivity() {
+        Activity activity = activityStack.lastElement();
+        finishActivity(activity);
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public void finishActivity(Activity activity) {
+        if (activity != null && !activity.isFinishing()) {
+            activityStack.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public void finishActivity(Class<?> cls) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass().equals(cls)) {
+                finishActivity(activity);
+                break;
+            }
+        }
+    }
+
+    /**
+     * 结束所有Activity
+     */
+    public void finishAllActivity() {
+        int size = activityStack.size();
+        for (int i=0; i<size; i++) {
+            activityStack.get(i).finish();
+        }
+        activityStack.clear();
+    }
+
+    /**
+     * 根据类名获取指定的Activity
+     * @param cls
+     * @return
+     */
+    public static Activity getActivity(Class<?> cls) {
+        if (activityStack != null) {
+            for (Activity activity : activityStack) {
+                if (activity.getClass().equals(cls)) {
+                    return activity;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void AppExit() {
+        try {
+            finishAllActivity();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
+        } catch (Exception e) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/robot0x/app/base/BaseApplication.java
@@ -0,0 +1,18 @@
+package com.robot0x.app.base;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+
+/**
+ * Created by Jackie on 2016/3/3.
+ */
+
+@SuppressLint("InflateParams")
+public class BaseApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
